83e54a1ac95fe3b2b63c35654204b7d624106937
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-20 00:01:27 -0500

    Agregar formulario FrmRegistrarClase para registrar nuevas clases

---
+++ FrmRegistrarClase.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace GimnasioApp
+{
+    partial class FrmRegistrarClase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombreClase = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.txtCapacidad = new System.Windows.Forms.TextBox();
+            this.cmbDificultad = new System.Windows.Forms.ComboBox();
+            this.btnRegistrarClase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de la clase:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descripción:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Duración (minutos)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dificultad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Capacidad";
+            // 
+            // txtNombreClase
+            // 
+            this.txtNombreClase.Location = new System.Drawing.Point(329, 46);
+            this.txtNombreClase.Name = "txtNombreClase";
+            this.txtNombreClase.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreClase.TabIndex = 5;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(329, 88);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Location = new System.Drawing.Point(329, 151);
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(100, 20);
+            this.txtDuracion.TabIndex = 7;
+            // 
+            // txtCapacidad
+            // 
+            this.txtCapacidad.Location = new System.Drawing.Point(329, 253);
+            this.txtCapacidad.Name = "txtCapacidad";
+            this.txtCapacidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCapacidad.TabIndex = 8;
+            // 
+            // cmbDificultad
+            // 
+            this.cmbDificultad.AllowDrop = true;
+            this.cmbDificultad.FormattingEnabled = true;
+            this.cmbDificultad.Items.AddRange(new object[] {
+            "Baja",
+            "",
+            "",
+            "Media",
+            "",
+            "",
+            "Alta"});
+            this.cmbDificultad.Location = new System.Drawing.Point(329, 201);
+            this.cmbDificultad.Name = "cmbDificultad";
+            this.cmbDificultad.Size = new System.Drawing.Size(121, 21);
+            this.cmbDificultad.TabIndex = 9;
+            // 
+            // btnRegistrarClase
+            // 
+            this.btnRegistrarClase.Location = new System.Drawing.Point(329, 324);
+            this.btnRegistrarClase.Name = "btnRegistrarClase";
+            this.btnRegistrarClase.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrarClase.TabIndex = 10;
+            this.btnRegistrarClase.Text = "Registrar clase";
+            this.btnRegistrarClase.UseVisualStyleBackColor = true;
+            this.btnRegistrarClase.Click += new System.EventHandler(this.btnRegistrarClase_Click);
+            // 
+            // FrmRegistrarClase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegistrarClase);
+            this.Controls.Add(this.cmbDificultad);
+            this.Controls.Add(this.txtCapacidad);
+            this.Controls.Add(this.txtDuracion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombreClase);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmRegistrarClase";
+            this.Text = "FrmRegistrarClase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombreClase;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.TextBox txtCapacidad;
+        private System.Windows.Forms.ComboBox cmbDificultad;
+        private System.Windows.Forms.Button btnRegistrarClase;
+    }
+}
---
+++ FrmRegistrarClase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+
+namespace GimnasioApp
+{
+    public partial class FrmRegistrarClase : Form
+    {
+        public FrmRegistrarClase()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrarClase_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreClase.Text) ||
+                string.IsNullOrWhiteSpace(txtDescripcion.Text) ||
+                string.IsNullOrWhiteSpace(txtDuracion.Text) ||
+                string.IsNullOrWhiteSpace(txtCapacidad.Text) ||
+                cmbDificultad.SelectedIndex == -1)
+            {
+                MessageBox.Show("Por favor completa todos los campos.");
+                return;
+            }
+
+            try
+            {
+                SqlConnection con = ConexionBD.ObtenerConexion();
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                string query = "INSERT INTO Clases (nombre_clase, Descripcion, Duracion_min, Dificultad, Capacidad) " +
+                               "VALUES (@Nombre, @Descripcion, @Duracion, @Dificultad, @Capacidad)";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Nombre", txtNombreClase.Text);
+                    cmd.Parameters.AddWithValue("@Descripcion", txtDescripcion.Text);
+                    cmd.Parameters.AddWithValue("@Duracion", int.Parse(txtDuracion.Text));
+                    cmd.Parameters.AddWithValue("@Dificultad", cmbDificultad.Text);
+                    cmd.Parameters.AddWithValue("@Capacidad", int.Parse(txtCapacidad.Text));
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Clase registrada correctamente.");
+                }
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar clase: " + ex.Message);
+            }
+        }
+
+    }
+}
