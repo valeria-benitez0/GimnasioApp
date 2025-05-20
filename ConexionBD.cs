f2155e713a89702a7a2f51abf71930e86ac0ade1
valeria.benitez889@pascualbravo.edu.co <valeria.benitez889@pascaulbravo.edu.co>
2025-05-19 21:34:54 -0500

    Agregar clase ConexionBD para manejo centralizado de conexión a SQL Server

---
+++ ConexionBD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+public class ConexionBD
+{
+    private static SqlConnection conexion;
+
+    private ConexionBD() { }
+
+    public static SqlConnection ObtenerConexion()
+    {
+        if (conexion == null)
+        {
+            conexion = new SqlConnection("Server=localhost;Database=GimnasioDB;Trusted_Connection=True;");
+        }
+        return conexion;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace GimnasioApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmRegistro());
         }
     }
 }
